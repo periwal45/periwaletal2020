43c1cffca33fb709ced5b70cc3a58b6258910be3
periwal <vinita.periwal@embl.de>
2020-08-31 11:47:09 +0200

    commit

--- README.md
+++ README.md
@@ -1,38 +1,35 @@
 # periwaletal2020
 periwaletal2020 source code
 
-link to input data: Periwal, Vinita (2020), “periwaletal drug-food similarity ML data”, Mendeley Data, V1, doi: 10.17632/7ft539gwf3.1
+link to input data files: Periwal, Vinita (2020), “periwaletal drug-food similarity ML data”, Mendeley Data, V1, doi: 10.17632/7ft539gwf3.1
 
 This repository contains scripts for manuscript periwaletal2020
 
 1. Data_preprocessing.R
 
-contains code to pre-process data as is described under 'Data preprocessing' section of manuscript
+contains code to pre-process data as is described under 'Data preprocessing' section of manuscript.
+
+-filters constant variables and removes NAs
+-train and test split
+-checks overlaps
 
 2. FeatureSelection.R and FV_filter_values.csv
 
 contains code to run feature importance as is described under 'Feature selection' section of manuscript. FV_filter_values contains the list of columns with their feature importance values
 
-3. Benchmark.R
+3. randomforest.R
 
-initial classification runs using ksvm, nnet, and random forest to assess their performance. Refer to section 'Random Forest classification' of the manuscript
+contains code for running the random forest classifier with default and all hyper-parameter settings. The code utilizes parallel computing in cluster environment. See manuscript section 'Random Forest classification'
 
-4. RandomForest_train_default.R
 
-contains code for running the classifier with default settings. See manuscript section 'Random Forest classification'
-
-5. RandomForest_train_hyp1.R 
-
-contains code for running the classifier with hyper-parameter settings 'hyp1'. See manuscript section 'Random Forest classification'
-
-6. RandomForest_train_hyp2.R
-
-contains code for running the classifier with hyper-parameter settings 'hyp2'. See manuscript section 'Random Forest classification'
-
-7. RandomForest_train_hyp3.R
-
-contains code for running the classifier with hyper-parameter settings 'hyp3'. See manuscript section 'Random Forest classification'
-
-8. Performance_Models.R
+4. Performance_Models.R
 
 This code compares the performance of all the trained models using the 20% test set. Performance was assessed using various statistical measures as described in the manuscript.
+
+5. FooDB_predictions.R 
+
+virtually screens the foodb compounds against the trained models and generates predictions. The FooDB dataset was split into smaller sized files.
+
+-virtual screening
+-consensus matching
+-adding annotations
--- randomforest.R
+++ randomforest.R
@@ -1,4 +1,4 @@
-# Building a random forest model
+# Building random forest models
 library(mlr)
 library(readr)
 library(dplyr)
@@ -14,7 +14,7 @@ train.set<-data.frame(read.csv("data_dist_desc_train", header = TRUE, sep = ',')
 train.set<-subset(train.set, select = -c(1))
 
 
-##Selecting importannt features
+##Selecting important features
 
 train.set<-subset(train.set, select = c(2,365,6,1,364,181,180,9,3,179,4,5,7,157,17,191,363,229,298,156,210,341,56,58,115,330,163,316,53,151,375,187,186,378,237,125,321,167,202,242,252,10,194,124,133,123,240,297,308,119,335,11,45,146,323,132,116,299,25,328,170,322,18,21,370,340,362,201,209,193,300,114,8,355,90,353,225,317,139,205,313,57,138,169,309,314,126,52,68,315,224,80,152,306,71,172,310,66,347,164,247,352,131,243,121,59,162,98,129,336,41,171,257,165,264,320,236,26,223,168,301,39,20,198,307,296,143,351,188,128,63,141,16,50,304,371,354,51,200,356,147,305,359,195,348,49,22,230,113,325,62,214,331,311,122,127,274,24,155,117,319,75,259,235,232,161,231,30,241,46,367,204,339,189,107,312,360,108,183,48,27,142,15,208,250,47,12,54,379))
 
@@ -29,7 +29,16 @@ rf.prob
 rf.prob <- makeWeightedClassesWrapper(rf.prob, wcw.param="classwt")
 rf.prob
 
-############################# 3. Optimization and Training (model building) #####################################
+## Default parameters
+
+# train a model
+rforest_dist_desc <- train(rf.prob, train_task_dist_desc)
+getLearnerModel(rforest_dist_desc)
+
+# save the model
+save(rforest_dist_desc, file = "rforest_default.rda")
+
+### Optimization and training (hyp1)
 
 # set tunable parameters
 
@@ -63,8 +72,74 @@ getLearnerModel(rforest_nonode)
 # save the model
 save(rforest_nonode, file = "rforest_hyp1.rda")
 
+### Optimization and training (hyp2)
+
+# set tunable parameters
+
+# 1. Specifying the search space of learner (RandomForest)
+rf_param <- makeParamSet(
+   makeIntegerParam("ntree",lower = 50, upper = 101),
+   makeIntegerParam("nodesize", lower = 30, upper = 150)
+)
 
 
+# 2. Specify optimization algorithm for search space: random search for 5 iterations
+rancontrol <- makeTuneControlRandom(maxit = 5L)
+# 3. Performing the tuning: set 3 fold cross validation
+set_cv <- makeResampleDesc("CV",iters = 3L,stratify=TRUE)
+
+# Setting parallel backend for hypertuning
+if(Sys.info()['sysname'] == "Linux") {
+   parallelStartMulticore(cpus = 5, show.info = T)
+} else parallelStartSocket(cpus = 5, show.info = T)
+
+# 4. Performing the hypertuning
+rf_tune_dist_desc <- tuneParams(learner = rf.prob, resampling = set_cv, task = train_task_dist_desc, par.set = rf_param, control = rancontrol, measures = list(mcc,tpr,fnr,fpr,tnr,acc,mmce,kappa)) 
+
+# 5. Using hyperparameters for modeling
+rf.tree_dist_desc <- setHyperPars(rf.prob, par.vals = rf_tune_dist_desc$x)
+
+# train a model
+rforest_nowt <- train(rf.tree_dist_desc, train_task_dist_desc)
+getLearnerModel(rforest_nowt)
+
+# save the models !
+save(rforest_nowt, file = "rforest_hyp2.rda")
+
+### Optimization and training (hyp3)
+
+# set tunable parameters
+
+# 1. Specifying the search space of learner (RandomForest)
+rf_param <- makeParamSet(
+   makeDiscreteParam("wcw.weight", seq(1000,30000,5000)),
+   makeIntegerParam("ntree", lower = 50, upper = 101),
+   makeIntegerParam("nodesize", lower =30, upper = 150)
+)
+
+
+# 2. Specify optimization algorithm for search space: random search for 5 iterations
+rancontrol <- makeTuneControlRandom(maxit = 5L)
+# 3. Performing the tuning: set 3 fold cross validation
+set_cv <- makeResampleDesc("CV", iters = 3L, stratify = TRUE)
+
+# Setting parallel backend for hypertuning
+if(Sys.info()['sysname'] == "Linux") {
+   parallelStartMulticore(cpus = 5, show.info = T)
+} else parallelStartSocket(cpus = 5, show.info = T)
+
+# 4. Performing the hypertuning
+rf_tune_dist_desc <- tuneParams(learner = rf.prob, resampling = set_cv, task = train_task_dist_desc, par.set = rf_param, control = rancontrol, measures = list(mcc,tpr,fnr,fpr,tnr,acc,mmce,kappa)) 
+
+# 5. Using hyperparameters for modeling
+rf.tree_dist_desc <- setHyperPars(rf.prob, par.vals = rf_tune_dist_desc$x)
+
+# train a model
+rforest_highcost <- train(rf.tree_dist_desc, train_task_dist_desc)
+getLearnerModel(rforest_highcost)
+
+# save the model
+save(rforest_highcost, file = "rforest_hyp3.rda")
 
 
 			  
