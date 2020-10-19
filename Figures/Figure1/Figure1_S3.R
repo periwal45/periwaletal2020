94f52eec47c22db4beaf69244eaa5f42ffa94aee
periwal <vin.peri04@gmail.com>
2020-10-19 13:59:44 +0200

    commit

--- Figures/Figure1/Figure1_S3.R
+++ Figures/Figure1/Figure1_S3.R
@@ -4,6 +4,7 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(ggsci)
+library(Cairo)
 
 # set global theme for all plots
 
@@ -30,7 +31,7 @@ nrow(data.reshape1) #6,953,415
 
 #Fig1B 
 
-CairoSVG(file="/Figure1/Fig1B.svg", width = 3.2, height = 3.5, bg = "white")
+CairoSVG(file="Figure1/Fig1B.svg", width = 3.2, height = 3.5, bg = "white")
 ggplot(data.reshape1,aes(x=variable, y=value))+geom_violin(adjust=3,fill="#bdbdbd", lwd=0.3)+
   geom_boxplot(width=0.1, outlier.shape = NA, fill="white", lwd=0.3)+
   scale_x_discrete(name = "Fingerprints", labels = c("Morgan", "FeatMorgan", "AtomPair","RDKit","Torsion","Layered","MACCS")) + 
@@ -49,11 +50,11 @@ data.reshape2<-melt(select_cols2, id.vars = "Final_dataset.drug_id")
 head(data.reshape2)
 nrow(data.reshape2) #1,986,690
 
-CairoSVG(file="/Figure1/Fig1C.svg", width = 3, height = 3.4, bg = "white")
+CairoSVG(file="Figure1/Fig1C.svg", width = 3, height = 3.4, bg = "white")
 ggplot(data.reshape2,aes(x=variable, y=value, fill=variable))+geom_violin(fill="#bdbdbd", lwd=0.3)+
   geom_boxplot(width=0.1, outlier.shape = NA, fill="white", lwd=0.3)+coord_flip()+
   scale_x_discrete(name = "Maximum Common Substructure", labels = c("TS", "OC")) + 
-  scale_y_continuous(name="value") + theme_minimal() + 
+  scale_y_continuous(name="Value") + theme_minimal() + 
   theme(plot.title = element_text(size = 14, face = "bold"),axis.title=element_text(size=14,color = "black"),axis.text.x = element_text(size=12, color = "black"),axis.text.y = element_text(size=12, color = "black"))+
   guides(fill=FALSE)
 dev.off()
@@ -64,21 +65,21 @@ fv_filter<-filter(fv, fv$feature_importance > 32)
 head(fv_filter)
 nrow(fv_filter)
 
-CairoSVG(file="/Figure1/Fig1D.svg", width = 5, height = 4, bg = "white")
+CairoSVG(file="Figure1/Fig1D.svg", width = 5, height = 4, bg = "white")
 ggplot(fv_filter, aes(reorder(name, -feature_importance), feature_importance)) + geom_bar(stat = "identity", fill = "#bdbdbd") +
-  xlab("Features (top 20)") + ylab("Features importance") +
+  xlab("Features (top 20)") + ylab("Feature importance") +
   theme(axis.text.x = element_text(angle = 90, hjust = 1), axis.line = element_line(size=0.3, colour = "black"), panel.grid = element_blank(), panel.background = element_blank(), axis.text = element_text(size = 12, colour = "black"), axis.title = element_text(size = 14, colour = "black"))
 dev.off()
 
 ##Fig 1E
-hyp<-data.frame(read.table(file="Models_TrainingMeasures", header = TRUE, sep = '\t'))
+hyp<-data.frame(read.table(file="Figure1/Models_TrainingMeasures", header = TRUE, sep = '\t'))
 head(hyp)
 melt.hyp<-melt(hyp, id.vars = c("Model","TPR","FNR","FPR","TNR","ACC","MMCE","Kappa"), measure.vars = c("TPR","FNR","FPR","TNR","ACC","MMCE","Kappa","MCC"))
 head(melt.hyp)
 
-CairoSVG(file="/Figure1/Fig1E.svg", width = 3.4, height = 3, bg = "white")
+CairoSVG(file="Figure1/Fig1E.svg", width = 3.4, height = 3, bg = "white")
 melt.hyp %>% dplyr::group_by(Model) %>% ggplot(aes(x=value,y=variable,fill=Model)) + 
-  geom_boxplot(lwd=0.3) + scale_fill_jama() + scale_y_discrete(name = "Measure") +
+  geom_boxplot(lwd=0.3) + scale_fill_jama() + scale_y_discrete(name = "Measure") + scale_x_continuous(name = "Value") +
   th + theme(legend.position = "bottom")
 dev.off()
 
@@ -99,13 +100,13 @@ df = generateThreshVsPerfData(list(Default = default_pred, Hyp1 = hyp1_pred, Hyp
 
 #Fig 1F
 #Precision/recall curve
-CairoSVG(file="/Figure1/Fig1F.svg", width = 3.2, height = 3.5, bg = "white")
+CairoSVG(file="Figure1/Fig1F.svg", width = 3.2, height = 3.5, bg = "white")
 plotROCCurves(df, measures = list(tpr,ppv), diagonal = FALSE)
 dev.off()
 
 #Fig 1G
 #ROC curve
-CairoSVG(file="/Figure1/Fig1G.svg", width = 3.2, height = 3.5, bg = "white")
+CairoSVG(file="Figure1/Fig1G.svg", width = 3.2, height = 3.5, bg = "white")
 plotROCCurves(df, measures = list(fpr,tpr))
 dev.off()
 
