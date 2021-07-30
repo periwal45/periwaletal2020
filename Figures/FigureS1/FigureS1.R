dc1ac4d231e36805d0eb1dc972d58d6814aa3a1e
periwal <vin.peri04@gmail.com>
2021-07-30 16:25:38 +0100

    commit

--- Figures/FigureS1/FigureS1.R
+++ Figures/FigureS1/FigureS1.R
@@ -4,6 +4,7 @@ library(tidyr)
 library(ggplot2)
 library(plyr) #for count
 library(ggsci)
+library(Cairo)
 
 ## Figure S1
 
@@ -18,7 +19,7 @@ head(count)
 nrow(count)
 
 #FigS1a
-CairoSVG(file="FigureS1/FigS1a.svg", width = 9, height = 6, bg = "white")
+CairoSVG(file="FigS1a.svg", width = 9, height = 6, bg = "white")
 ggplot(data.frame(count), aes(x = Superclass, y = freq, group=ATC, fill=ATC)) + geom_point(size =2.5, shape=23, position = position_dodge(width = 0.4))  + scale_fill_d3(palette = "category20") + scale_y_continuous(trans='log10') + labs(x = "Structural Classification (Superclass)", y = "Freq (log10)")+theme_bw()+theme(axis.text = element_text(size = 12, colour = "black"), axis.title = element_text(size = 12), axis.text.x = element_text(angle = 90, hjust = 1), legend.text = element_text(size = 12), legend.title = element_text(size = 14)) + coord_flip()
 dev.off()
 
@@ -30,6 +31,6 @@ fcount<-count(data, c('Superclass','Type'))
 head(fcount)
 
 #FigS1b
-CairoSVG(file="FigureS1/FigS1b.svg", width = 9, height = 6, bg = "white")
+CairoSVG(file="FigS1b.svg", width = 9, height = 6, bg = "white")
 ggplot(data.frame(fcount), aes(x = Superclass, y = freq, group=Type, fill=Type)) + geom_point(size =2.5, shape=23, position = position_dodge(width = 0.4))  + scale_fill_d3(palette = "category20") + scale_y_continuous(trans='log10') + labs(x = "Structural Classification (Superclass)", y = "Freq (log10)")+theme_bw()+theme(axis.text = element_text(size = 12, colour = "black"), axis.title = element_text(size = 12), axis.text.x = element_text(angle = 90, hjust = 1), legend.text = element_text(size = 12), legend.title = element_text(size = 14)) + coord_flip()
 dev.off()
