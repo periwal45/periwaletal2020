d96c33d8084b72929a8b9d2645a8ecaa5b140739
periwal <vin.peri04@gmail.com>
2022-01-31 21:09:22 +0000

    commit

--- food_predictions.R
+++ food_predictions.R
@@ -11,9 +11,10 @@ DvF<-DvF %>% filter(f1_morgan < 0.9 & f1_featmorgan < 0.9 & f1_atompair < 0.9 & 
 dim(DvF)
 
 # load model
-load("wt_rforest.rda")
+wt300_rf<-load("train_dist_desc_set1_wt_rforestT.rda") #files on mendeley link
+print("set1 model")
+get(wt300_rf)
 
-pred_DvF.set = predict(wt_rforest, newdata = DvF, type = "prob")
-pred_set = setThreshold(pred_DvF.set,0.6)
-predictions<-data.frame(DvF$Final_dataset.drug_id,as.data.frame(pred_set))
-write.csv(predictions, file="drug_food_preds.csv", row.names = FALSE)
+pred_DvF.set1 = predict(get(wt300_rf), newdata = DvF, type = "prob")
+pred_DvF<-data.frame(DvF$Final_dataset.drug_id,as.data.frame(pred_DvF.set1))
+write.csv(pred_DvF, file="set1rf_drug_food_preds.csv", row.names = FALSE)
