e390e4ee37390327870a5d72feeee689a6fa52b9
periwal <vin.peri04@gmail.com>
2022-01-31 16:44:34 +0000

    commit

--- Figures/Figure2/Figure2a.R
+++ Figures/Figure2/Figure2a.R
@@ -1,7 +1,8 @@
-# creating a scatter plot to show predictions from best fingerprint and ML model
+# drug food predictions by all 5 models
 library(reshape2)
 library(dplyr)
 library(tidyr)
+library(tidyverse)
 library(ggplot2)
 library(ggsci)
 library(Cairo)
@@ -12,25 +13,31 @@ th<-theme(plot.title = element_text(size = 12, face = "bold"),axis.title=element
           axis.text.x = element_text(size=10, color = "black"),axis.text.y = element_text(size=10, color = "black"))
 
 
-# plotting food predictions different threshold
+# predictions
+set1_food<-data.frame(read.csv(file = "set1_drug_food.csv", header = FALSE))
+set2_food<-data.frame(read.csv(file = "set2_drug_food.csv", header = FALSE))
+set3_food<-data.frame(read.csv(file = "set3_drug_food.csv", header = FALSE))
+set4_food<-data.frame(read.csv(file = "set4_drug_food.csv", header = FALSE))
+set5_food<-data.frame(read.csv(file = "set5_drug_food.csv", header = FALSE))
 
-food_preds<-data.frame(read.table(file = "drug_food_preds.csv", header = TRUE, sep = ','))
-head(food_preds)
-nrow(food_preds)
+names(set1_food)[1]<-"Final_dataset.drug_id"
+names(set2_food)[1]<-"Final_dataset.drug_id"
+names(set3_food)[1]<-"Final_dataset.drug_id"
+names(set4_food)[1]<-"Final_dataset.drug_id"
+names(set5_food)[1]<-"Final_dataset.drug_id"
 
-prob.5<-nrow(food_preds %>% filter(prob.Match > 0.5))
-prob.6<-nrow(food_preds %>% filter(prob.Match > 0.6))
-prob.7<-nrow(food_preds %>% filter(prob.Match > 0.7))
-prob.8<-nrow(food_preds %>% filter(prob.Match > 0.8))
-prob.9<-nrow(food_preds %>% filter(prob.Match > 0.9))
+sets<-c("set1","set2","set3","set4","set5")
+value<-c(676,826,911,735,617)
+df<-data.frame(sets,value)
+head(df)
 
-counts<-data.frame(prob.5,prob.6,prob.7,prob.8,prob.9)
-counts
-
-CairoSVG(file="prob.svg", width = 3, height = 2, bg = "white")
-counts %>% melt %>% ggplot(aes(x=variable, y=value)) + geom_bar(stat = "identity") + 
-  th + scale_x_discrete(name = "probability cut off", labels=c("prob.5" = "> 0.5","prob.6" = "> 0.6","prob.7" = "> 0.7","prob.8" = "> 0.8","prob.9" = "> 0.9")) + 
+CairoSVG(file="Fig2a.svg", width = 3, height = 2, bg = "white")
+df %>% ggplot(aes(x=sets,y=value)) + geom_bar(stat = "identity") +
+  th + scale_x_discrete(name = "set") + 
   scale_y_continuous(name = "count") +
-  geom_text(aes(label=value), color="black", size = 3, nudge_y = 30)
+  geom_text(aes(label=value), color="black", size = 3, nudge_y = 40)
 dev.off()
 
+
+# command line count of each pair
+#sort all_set_DvF | uniq -c > count_of_pairs
